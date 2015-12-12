106b8eb1376d11d1e9cfdd4671827e986a957dbb
CrashLearner <yonayen@hotmail.com>
2015-12-12 09:34:33 -0800

    User_id to Items Table and Associations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
 	end
 
 	def new
-		@item = Item.new
+		@item = current_user.items.build
 	end
 
 	def create
-		@item = Item.new(item_params)
+		@item = current_user.items.build(item_params)
 		if @item.save
 			redirect_to root_path
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212002916) do
+ActiveRecord::Schema.define(version: 20151212172832) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
